2d933f7ee58a70f4a89c46013fa7baefb0f95a57
Florian <florian_swienty@hotmail.de>
2019-11-13 16:47:44 +0100

    some communication progress

--- pc_control/control.py
+++ pc_control/control.py
@@ -1,8 +1,6 @@
 from cflib.drivers.crazyradio import Crazyradio
 import time
 
-cr = Crazyradio(devid=0)  # maybe 1, idk
-
 
 def start():
     print("start")
@@ -12,6 +10,17 @@ def land():
     print("land")
 
 
+def send():
+    cr.send_packet((0xff, 0x80, 0x63, 0x01))
+    print('send')
+
+
+
+cr = Crazyradio(devid=0)  # maybe 1, idk
+cr.set_channel(80)
+cr.set_data_rate(cr.DR_2MPS)
+cr.set_address((0xe7, 0xe7, 0xe7, 0xe7, 0xe4))
+cr.set_ack_enable(True)
 
 while True:
     inp = input()
@@ -19,6 +28,8 @@ while True:
         start()
     elif inp == "land":
         land()
+    elif inp == "send":
+        send()
     elif inp == "quit":
         break
     else:
--- src/random.c
+++ src/random.c
@@ -11,11 +11,11 @@
 
 #include "debug.h"
 
+#include "param.h"
 #include "log.h"
 
 // my stuff
 #include "estimator_kalman.h"
-
 
 #define DEBUG_MODULE "PUSH"
 
@@ -53,27 +53,73 @@ void appMain()
   static setpoint_t setpoint;
   static point_t kalmanPosition;
 
-  vTaskDelay(M2T(3000)); // wait x ms, M2T: ms to os ticks
+  static float myNumber = 0;
+  
+  /*    change this number via the crazyradio to make the crazyflie do stuff
+  *     0: nothing
+  *     1: start
+  *    -1: land
+  */
+  static int8_t command = 0;
+
+  static float posX;
+  static float posY;
+  static float posZ;
+
+  vTaskDelay(M2T(500)); // wait x ms, M2T: ms to os ticks
   resetEstimator();
-  vTaskDelay(M2T(10000));
+  vTaskDelay(M2T(500));
 
   estimatorKalmanGetEstimatedPos(&kalmanPosition);
   //uint16_t idUp = logGetVarId("range", "up");
 
   DEBUG_PRINT("Waiting for activation ...\n");
 
+  LOG_GROUP_START(test);
+  LOG_ADD(float, myNumber, &myNumber)
+  LOG_GROUP_STOP(test);
+
+  PARAM_GROUP_START(command)
+  PARAM_ADD(PARAM_INT8, command, &command)
+  PARAM_GROUP_STOP(command)
+
   while (1)
   {
     vTaskDelay(M2T(10));
+
+    // get current position
+    varid = logGetVarId("kalman", "stateX");
+    posX = logGetFloat(varid);
+    varid = logGetVarId("kalman", "stateY");
+    posY = logGetFloat(varid);
+    varid = logGetVarId("kalman", "stateZ");
+    posZ = logGetFloat(varid);
+
+    switch (command)
+    {
+    case 0:
+      // if the crazyflie should do something when flying normally, put it here
+      break;
+    case 1:
+      // start code
+      break;
+    case -1:
+      //land code
+      break;
+    default:
+      break;
+    }
+    command = 0;
+
     if (1)
     {
       setHoverSetpoint(&setpoint, 0, 0, .3, 0);
-      commanderSetSetpoint(&setpoint, 3);
+      //commanderSetSetpoint(&setpoint, 3);
+      myNumber += 0.01;
     }
   }
 }
 
 void waitForPositionEstimator()
 {
-
 }
