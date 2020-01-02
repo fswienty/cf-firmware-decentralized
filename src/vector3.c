74bcd380a51a94adcbfd5bf0eeb8bd89ffcf09e1
vrojak <florian_swienty@hotmail.de>
2020-01-02 12:44:09 +0100

    Update vector3.c

--- src/vector3.c
+++ src/vector3.c
@@ -2,11 +2,6 @@
 
 Vector3 add(Vector3 a, Vector3 b)
 {
-    // Vector3 result;
-    // result.x = a.x + b.x;
-    // result.y = a.y + b.y;
-    // result.z = a.z + b.z;
-    // return result;
     return (Vector3){
         a.x + b.x,
         a.y + b.y,
@@ -17,11 +12,6 @@ Vector3 add(Vector3 a, Vector3 b)
 // The resulting Vector3 points from 'from' to 'to'.
 Vector3 sub(Vector3 from, Vector3 to)
 {
-    // Vector3 result;
-    // result.x = to.x - from.x;
-    // result.y = to.y - from.y;
-    // result.z = to.z - from.z;
-    // return result;
     return (Vector3){
         to.x - from.x,
         to.y - from.y,
@@ -31,11 +21,6 @@ Vector3 sub(Vector3 from, Vector3 to)
 
 Vector3 mul(Vector3 vec3, float f)
 {
-    // Vector3 result;
-    // result.x = vec3.x * f;
-    // result.y = vec3.y * f;
-    // result.z = vec3.z * f;
-    // return result;
     return (Vector3){
         vec3.x * f,
         vec3.y * f,
@@ -51,10 +36,5 @@ float magnitude(Vector3 vec3)
 Vector3 norm(Vector3 vec3)
 {
     float magInv = 1 / magnitude(vec3);
-    // Vector3 result;
-    // result.x = vec3.x * magInv; 
-    // result.y = vec3.y * magInv;
-    // result.z = vec3.z * magInv;
-    // return result;
     return mul(vec3, magInv);
 }
